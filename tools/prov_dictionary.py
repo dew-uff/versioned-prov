6f1c0febd1726cfa76bb6a20c0d1be79169fa369
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2018-02-22 02:07:26 -0300

    Add prov-dictionary mapping

--- tools/prov_dictionary.py
+++ tools/prov_dictionary.py
@@ -1,4 +1,25 @@
-from provn import graph, _arrow2, _arrow3, _labeldict
+from provn import graph
+
+def _dlabeldict(label):
+	return {
+        "labelfontsize": "8",
+        "labeldistance": "1.5",
+        "color": "darkgreen",
+        "labelangle": "60.0",
+        "rotation": "20",
+        "label": label
+    }
+
+
+def _darrow2(dot, first, second, label="", extra={}):
+    if not first or not second:
+        return None
+
+    url1 = dot.prefix(first)
+    url2 = dot.prefix(second)
+
+    return dot.arrow(url1, url2, attrs=dict(**_dlabeldict(label), **extra))
+
 
 def _unquote(key):
     if key and key.startswith('"') and key.endswith('"'):
@@ -9,7 +30,7 @@ def _unquote(key):
 def had_dictionary_member(dot, did=None, eid=None, key=None, attrs=None, id_=None):
     if key and key.startswith('"') and key.endswith('"'):
         key = key[1:-1]
-    return _arrow2(dot, did, eid, "[{}]".format(key), extra={"color": "darkgreen"})
+    return _darrow2(dot, did, eid, "[{}]".format(key))
 
 @graph.prov("derivedByInsertionFrom")
 def derived_by_insertion_from(dot, dgen=None, duse=None, changes=None, attrs=None, id_=None):
@@ -26,7 +47,7 @@ def derived_by_insertion_from(dot, dgen=None, duse=None, changes=None, attrs=Non
     if duse:
         use_url = dot.prefix(duse)
         result += "\n" + dot.arrow(
-            point, use_url, attrs=dict(**_labeldict("der"), **{"color": "darkgreen"})
+            point, use_url, attrs=_dlabeldict("der")
         )
 
     for key, ent in changes:
@@ -35,7 +56,7 @@ def derived_by_insertion_from(dot, dgen=None, duse=None, changes=None, attrs=Non
         if ent:
             ent_url = dot.prefix(ent)
             result += "\n" + dot.arrow(
-                point, ent_url, attrs=dict(**_labeldict("[{}]".format(key)), **{"color": "darkgreen"})
+                point, ent_url, attrs=_dlabeldict("[{}]".format(key))
             )
     return result
 
@@ -44,7 +65,7 @@ def derived_by_insertion_from(dot, dgen=None, duse=None, removals=None, attrs=No
     tremovals = ", ".join(
         "[{}]".format(_unquote(x)) for x in removals if x
     )
-    return _arrow2(dot, dgen, duse, "der-rem\n{}".format(tremovals), extra={"color": "darkgreen"})
+    return _darrow2(dot, dgen, duse, "der-rem\n{}".format(tremovals))
 
 if __name__ == "__main__":
     graph.main()
