446af4c4647fac70bf34e3417c880e066212c02e
João Felipe Pimentel <joaofelipenp@gmail.com>
2018-02-24 18:25:26 -0300

    Change Prov-Dictionary

--- algorithm.py
+++ algorithm.py
@@ -7,11 +7,9 @@ result = dist = [
 nodes = len(dist)
 indexes = range(nodes)
 for k in indexes:
-    distk = dist[k]
     for i in indexes:
-        disti = dist[i]
         for j in indexes:
-            ikj = disti[k] + distk[j]
-            if disti[j] > ikj:
-                disti[j] = ikj
+            ikj = dist[i][k] + dist[k][j]
+            if dist[i][j] > ikj:
+                dist[i][j] = ikj
 print(result[0][2])
